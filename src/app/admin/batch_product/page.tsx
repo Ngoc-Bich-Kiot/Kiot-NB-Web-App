0c0095f8d58cce301bfc8135acd98e578267ca5d
rejavik <datha114677@gmail.com>
2025-06-04 19:22:36 +0700

    metadata

--- src/app/admin/batch_product/page.tsx
+++ src/app/admin/batch_product/page.tsx
@@ -1,5 +1,11 @@
 import TableBatch from "@/container/ManageBatch/TableBatch";
-import React from "react";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Quản lý lô sản phẩm",
+  description:
+    "Trang quản trị để quản lý lô sản phẩm, bao gồm thêm, sửa và xóa lô sản phẩm.",
+};
 
 const BatchProductPage = async () => {
   await new Promise((resolve) => setTimeout(resolve, 1000));
--- src/app/admin/manage_category/page.tsx
+++ src/app/admin/manage_category/page.tsx
@@ -1,5 +1,11 @@
 import CategoryTable from "@/container/category/CategoryTable";
-import React from "react";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Quản lý danh mục sản phẩm",
+  description:
+    "Trang quản trị để quản lý danh mục sản phẩm, bao gồm thêm, sửa và xóa danh mục.",
+};
 
 const ManageCategoryPage = async () => {
   await new Promise((resolve) => setTimeout(resolve, 1000));
